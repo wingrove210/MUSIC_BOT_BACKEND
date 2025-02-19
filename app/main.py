d6e10e042a29e062fad6e38e305b20dedd6d5f3f
Vlad Dremkov <vladidermkov330@gmail.com>
2025-02-20 01:30:56 +0300

    modified:   app/__pycache__/main.cpython-313.pyc 	modified:   app/main.py

--- app/main.py
+++ app/main.py
@@ -35,4 +35,4 @@ Base.metadata.create_all(bind=engine)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
