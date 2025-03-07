b460c739bdb34a318737daa73c99a6b4f228dc39
Merdan-Mahmudow <softprank@gmail.com>
2025-03-07 16:38:14 +0300

    deleted:    .env 	modified:   .gitignore 	modified:   app/routers/track.py 	new file:   app/services/__init__.py 	new file:   app/services/files.py 	modified:   pyproject.toml 	modified:   uv.lock
    Changes not staged for commit:
    	modified:   .dockerignore
    	modified:   Dockerfile
    	modified:   app/app.py
    	modified:   app/core/config.py
    	modified:   app/database.py

--- app/routers/track.py
+++ app/routers/track.py
@@ -4,8 +4,7 @@ from sqlalchemy import delete, insert, select
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.database import get_async_session
 from app.models.track import Track
-from app.schemas.track import TrackResponse, TrackCreate
-from fastapi.responses import FileResponse
+from app.services.files import get_url, get_url_music
 
 UPLOAD_DIR = "uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
@@ -17,12 +16,13 @@ async def create_track(
     name: str = Form(...),
     artist: str = Form(...),
     duration: str = Form(...),
-    image: str = Form(...),
+    image: UploadFile = File(...),
     file: UploadFile = File(...),
     db: AsyncSession = Depends(get_async_session),
 ):
     file_path = f"{UPLOAD_DIR}/{file.filename}"
-
+    url_image = await get_url([image])
+    url_music = await get_url_music([file])
     # Save file
     with open(file_path, "wb") as buffer:
         buffer.write(await file.read())
@@ -32,15 +32,15 @@ async def create_track(
         name=name,
         artist=artist,
         duration=duration,
-        url=file_path,
-        image=image,
+        url=url_music[0],
+        image=url_image[0],
     )
 
     query = insert(Track).values(name=name,
                                  artist=artist,
                                  duration=duration,
-                                 url=file_path,
-                                 image=image,)
+                                 url=url_music[0],
+                                 image=url_image[0],)
     await db.execute(query)
     await db.commit()
     return new_track
@@ -64,20 +64,6 @@ async def get_track(track_id: int, db: AsyncSession = Depends(get_async_session)
     if not track:
         raise HTTPException(status_code=404, detail="Track not found")
     return track
-
-# @track_router.get("/tracks/{track_id}/download")
-# async def download_track(track_id: int, db: AsyncSession = Depends(get_async_session)):
-#     query = select(Track).where(Track.id == track_id)
-#     result = await db.execute(query)
-#     track = result.scalar_one_or_none()
-#     if not track:
-#         raise HTTPException(status_code=404, detail="Track not found")
-
-#     file_path = track.url
-#     if not os.path.exists(file_path):
-#         raise HTTPException(status_code=404, detail="File not found")
-
-#     return FileResponse(path=file_path, filename=os.path.basename(file_path), media_type='audio/mpeg')
 
 @track_router.delete("/tracks/{track_id}")
 async def delete_track(track_id: int, db: AsyncSession = Depends(get_async_session)):
